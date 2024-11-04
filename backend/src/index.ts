9bc92a3cc1819e0b8aa4866fad5d0cce7d8192f9
sujalcharati <sujalc024@gmail.com>
2024-11-04 19:37:42 +0530

    migration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,26 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate())
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.post('/api/v1/singup', (c) => {
+  return c.text('Hello Hono!')
+})
+app.post('/api/v1/singin', (c) => {
+  return c.text('Hello Hono!')
+})
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
