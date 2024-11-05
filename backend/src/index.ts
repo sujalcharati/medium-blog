227a75f5047cfe83d696a78f42a4696e970f5747
sujalcharati <sujalc024@gmail.com>
2024-11-05 11:43:07 +0530

    signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,31 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-
-const prisma = new PrismaClient({
-    datasourceUrl: env.DATABASE_URL,
-}).$extends(withAccelerate())
-
+import { sign  } from 'hono/jwt'
 const app = new Hono()
 
-app.post('/api/v1/singup', (c) => {
-  return c.text('Hello Hono!')
+
+app.post('/api/v1/singup', async(c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore
+      datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data:{
+      email: body.email,
+      password :body.password,
+    },
+  })
+  //@ts-ignore
+  const token = await sign({
+    id: user.id}, c.env.JWT_SECRET)
+    
+  return c.json({
+    jwt:token
+  })
+
 })
 app.post('/api/v1/singin', (c) => {
   return c.text('Hello Hono!')
