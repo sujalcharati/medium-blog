d480d89a6179f9eaee6a8c155ebb78b0c0b800f1
sujalcharati <sujalc024@gmail.com>
2024-11-06 23:12:32 +0530

    middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,25 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign  } from 'hono/jwt'
+import { sign, verify  } from 'hono/jwt'
 const app = new Hono()
+
 app.use('/api/v1/blog/*', async (c, next) => {
-  await next()
+  const header = c.req.header("authorization") || "";
+  const token = header.split('')[1]
+  //@ts-ignore
+  const response = verify(token,c.env.JWT_SECRET);
+  //@ts-ignore
+  if (response.id) {
+    next()
+  }else
+  {
+ c.status(403)
+ return c.json({
+  error:"unauthorized"
+ })
+  }
+
 })
 
 app.post('/api/v1/singup', async(c) => {
