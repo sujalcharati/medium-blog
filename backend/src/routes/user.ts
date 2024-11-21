f20274ab1b9dc35cf38e6b1828682c8804b4c96c
sujalcharati <sujalc024@gmail.com>
2024-11-21 22:40:08 +0530

    update

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -43,7 +43,7 @@ userRouter.post("/signup", async (c) => {
  
 });
 
-userRouter.post("/singin", async (c) => {
+userRouter.post("/signin", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
